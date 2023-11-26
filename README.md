52db791d70267cb7d464d81f4dbadb04c637ca68
pengu <mflakowski.contact@gmail.com>
2023-11-26 14:00:10 +0100

    implement sample generator

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 All tasks should be performed from `src` directory.
 
+## Components
+
+### `main`
+
+* Reads arrays to sort from a source file.
+* Sorts arrays using different implementations of *merge sort* algorithm.
+* Benchmarks the execution of each method.
+* Saves the results to the target file.
+
+### `samplegen`
+
+* Generates random arrays to be sorted.
+* Saves them to the target file.
+
 ## Compiling
 
 ### `main`
@@ -32,6 +46,20 @@ make clean
 
 ### `samplegen`
 
+* **arg1:** number of arrays to generate
+* **arg2:** length of each array
+* **arg3:** output file name
+
 ```shell
-./samplegen
+./samplegen 100 100 output.txt
+```
+
+Output file format is as follows:
+
+```
+<number_of_arrays_to_generate> <length_of_each_array>
+arr1_element1 arr1_element2 arr1_element3 ...
+arr2
+arr3
+...
 ```
--- src/samplegen.c
+++ src/samplegen.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("samplegen\n");
+void generateSamples(int arraysToGenerate, int lengthOfArray, char* outputFile) {
+    srand((unsigned int)time(NULL));
+    FILE *fp;
+    fp = fopen(outputFile, "w");
+    fprintf(fp, "%d %d\n", arraysToGenerate, lengthOfArray);
+    for (int arrayNum = 0; arrayNum < arraysToGenerate; arrayNum++) {
+        for (int i = 0; i < lengthOfArray; i++) {
+            int r = rand();
+            fprintf(fp, "%d ", r);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Insufficient number of args:\n"
+            "arg1: number of arrays to generate\n"
+            "arg2: length of each array\n"
+            "arg3: output file name");
+        return 1;
+    }
+    int arraysToGenerate = atoi(argv[1]);
+    int lengthOfArray = atoi(argv[2]);
+    char* outputFile = argv[3];
+    printf("Sample generator:\n"
+        "\tArrays to generate: %d\n"
+        "\tLength of each array: %d\n"
+        "\tOutput file: %s\n",
+        arraysToGenerate, lengthOfArray, outputFile);
+    generateSamples(arraysToGenerate, lengthOfArray, outputFile);
+    printf("done");
     return 0;
 }
